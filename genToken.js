daad30c32b162f0d1eb6f5e35cb6b30b175c5989
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-12-19 20:17:16 +0530

    Genarate Token Update
    Now token will also saved on a file named session.json . Some people are getting problem to copy the Session. session.json will be used if you are not using enviroment variable. Please delete session.json after you copy it and not gonna use that.

--- genToken.js
+++ genToken.js
@@ -1,5 +1,6 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const fs = require('fs');
 
 const client = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] } });
 client.initialize();
@@ -11,5 +12,16 @@ client.on('qr', (qr) => {
 
 client.on('authenticated', (session) => {
     console.log(JSON.stringify(session))
-    process.exit()
+    fs.readFile(__dirname + '/session.json', { encoding: 'utf8' }, function(err, data) {
+        if (err) {
+            fs.writeFileSync(__dirname + '/session.json', JSON.stringify(session))
+            console.log("\n\nToken also saved on a file named session.json in this directory. Please delete this file after copy if you will use enviroment variable.")
+            process.exit()
+        } else {
+            fs.unlinkSync(__dirname + '/session.json')
+            fs.writeFileSync(__dirname + '/session.json', JSON.stringify(session))
+            console.log("\n\nToken also saved on a file named session.json in this directory. Please delete this file after copy if you will use enviroment variable.")
+            process.exit()
+        }
+    })
 });

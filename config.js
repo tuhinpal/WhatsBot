bb736450330753d717889b4aad35af989aa7aa33
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-12-19 15:18:42 +0530

    Added Procfile, added APT buildpack

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-// Only Require if you are not using env vars
+// Add values if you are not using env vars
 const fs = require('fs');
 
 module.exports = {
-    session: JSON.parse(process.env.SESSION || fs.readFileSync(__dirname + '/session.json', { encoding: 'utf8' })), //if not using config vars create a file and use
+    session: JSON.parse(process.env.SESSION || fs.readFileSync(__dirname + '/session.json', { encoding: 'utf8' })), //if not using env vars create a file named session.json
     pmpermit_enabled: process.env.PMPERMIT_ENABLED || "true",
     mongodb_url: process.env.MONGODB_URL || "",
     pmpermit_mutetime: process.env.PMPERMIT_MUTETIME || "1800",

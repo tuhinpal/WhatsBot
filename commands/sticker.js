b49a66fa11556d2e9b87e0721a5aeb72840fbc5a
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-09-12 22:31:37 +0530

    sticker command

---
+++ commands/sticker.js
@@ -0,0 +1,20 @@
+//jshint esversion:8
+const {MessageMedia} = require('whatsapp-web.js');
+
+const execute = async (client,msg) => {
+    msg.delete(true);
+    let quotedMsg = await msg.getQuotedMessage();
+    if (quotedMsg.hasMedia) {
+        let attachmentData = await quotedMsg.downloadMedia();
+        await client.sendMessage(msg.to, new MessageMedia(attachmentData.mimetype, attachmentData.data, attachmentData.filename), { sendMediaAsSticker: true });
+    } else {
+        await client.sendMessage(msg.to, `🙇‍♂️ *Error*\n\n` + "```No image found to make a Sticker```");
+    }
+};
+
+module.exports = {
+    name: 'Sticker Maker',
+    description: 'generates sticker from image',
+    command: '!sticker',
+    help: '',
+    execute};

543a02398bbea01653ec3659c6319549c1dbea45
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-09-14 20:57:26 +0530

    Added TTS Command 🥳

--- modules/tts.js
+++ modules/tts.js
@@ -1,1 +1,19 @@
-let googleTTS = require('google-translate-tts');
+const googleTTS = require('google-translate-tts');
+
+async function mainF(text) {
+
+          const buffer = await googleTTS.synthesize({
+            text: ${text},
+            voice: ${text.replace('\\ { }')}
+        });
+
+    return ({
+        mimetype: "audio/mp3",
+        data: buffer,
+        filename: "tts"
+    })
+}
+
+module.exports = {
+    mainF
+}

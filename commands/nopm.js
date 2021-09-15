b080ac155800c11c4d0fb7acb6d0c33d7ea7659d
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-09-15 17:40:43 +0530

    pmpermit trial

---
+++ commands/nopm.js
@@ -0,0 +1,20 @@
+//jshint esversion:8
+const config = require('../config');
+const pmpermit = require('../modules/pmpermit');
+
+const execute = async (client,msg) => {
+    if (config.pmpermit_enabled == "true" && !msg.to.includes("-")) {
+        pmpermit.nopermitacton(msg.to.split("@")[0]);
+        msg.reply("Not Allowed for PM");
+    }
+    
+};
+
+module.exports = {
+    name: 'Disallow PM', //name of the module
+    description: 'Disallow an allowed user for PM', // short description of what this command does
+    command: '!nopm', //command with prefix. Ex command: '!test'
+    commandType: 'admin', //
+    isDependent: true, //whether this command is related/dependent to some other command
+    help: 'This command is related to !allow. Type !help allow to learn about this', // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+    execute};
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const { Client } = require('whatsapp-web.js');
+const pmpermit = require('./modules/pmpermit');
 const config = require('./config');
 const fs = require("fs");
 
@@ -31,6 +32,38 @@ client.on('ready', () => {
     console.log('Bot has been started');
 });
 
+client.on('message', async msg => {
+    if (msg.author == undefined && config.pmpermit_enabled == "true") { // Pm check for pmpermit module
+        let pmpermitcheck = await pmpermit.handler(msg.from.split("@")[0]);
+        const chat = await msg.getChat();
+        if (pmpermitcheck == "permitted") {
+            // do nothing
+        } else if (pmpermitcheck.mute == true && chat.isMuted == false) { // mute 
+            msg.reply(pmpermitcheck.msg);
+            const chat = await msg.getChat();
+
+            let unmuteDate = new Date();
+            unmuteDate.setSeconds(Number(unmuteDate.getSeconds()) + Number(config.pmpermit_mutetime));
+            await chat.mute(unmuteDate);
+
+        } else if (chat.isMuted == true) {
+            //do nothing
+        } else if (pmpermitcheck == "error") {
+            //do nothing
+        } else {
+            msg.reply(pmpermitcheck.msg);
+        }
+
+    } else {
+        if (msg.body.includes("!info")) {
+
+            let startdata = await start.get(await client.info.getBatteryStatus(), client.info.phone);
+            client.sendMessage(msg.to, new MessageMedia(startdata.mimetype, startdata.data, startdata.filename), { caption: startdata.msg });
+
+        }
+    }
+});
+
 client.on('message_create', async msg => {
     if(msg.fromMe && msg.body.startsWith('!')) {
         let args = msg.body.slice(1).trim().split(/ +/g);
@@ -51,4 +84,28 @@ client.on('message_create', async msg => {
             console.log(client.commands);
         }
     }
+});
+
+client.on('message_revoke_everyone', async (after, before) => {
+    if (before) {
+        if (before.fromMe !== true && before.hasMedia !== true && before.author == undefined && config.enable_delete_alert == "true") {
+            client.sendMessage(before.from, "_You deleted this message_ 👇👇\n\n" + before.body);
+        }
+    }
+});
+
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
+
+app.get('/', (req, res) => {
+    res.send('<h1>This server is powered by Whatsbot<br><a href="https://github.com/TheWhatsBot/WhatsBot">https://github.com/TheWhatsBot/WhatsBot</a></h1>');
+});
+
+app.use('/public', express.static('public'), serveIndex('public', { 'icons': true })); // public directory will be publicly available
+
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server listening at Port: ${process.env.PORT || 8080}`);
 });

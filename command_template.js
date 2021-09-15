91319de079d71406c4e989aecd99a380b98b20c9
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-09-15 23:18:58 +0530

    minor fixes

--- command_template.js
+++ command_template.js
@@ -7,7 +7,7 @@ module.exports = {
     name: '', //name of the module
     description: '', // short description of what this command does
     command: '', //command with prefix. Ex command: '!test'
-    commandType: '', //
+    commandType: '', // admin|info|plugin
     isDependent: true | false, //whether this command is related/dependent to some other command
     help: '', // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
     execute};

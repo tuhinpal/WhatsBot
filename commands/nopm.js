9740121d5212aa20a92440194b5bd22be4adbcec
Anshuman Mahato <58422570+AnshumanMahato@users.noreply.github.com>
2021-09-16 16:11:25 +0530

    nopm is independent

--- commands/nopm.js
+++ commands/nopm.js
@@ -12,9 +12,9 @@ const execute = async (client,msg) => {
 
 module.exports = {
     name: 'Disallow PM', //name of the module
-    description: 'Disallow an allowed user for PM', // short description of what this command does
+    description: 'Disallow an allowed user for Personal messaging', // short description of what this command does
     command: '!nopm', //command with prefix. Ex command: '!test'
     commandType: 'admin', //
     isDependent: false, //whether this command is related/dependent to some other command
-    help: 'This command is related to !allow. Type !help allow to learn about this', // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
+    help: 'Type !nopm in the chat to execute.', // a string descring how to use this command Ex = help : 'To use this command type !test arguments'
     execute};
